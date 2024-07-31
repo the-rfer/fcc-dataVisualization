498627652686494751b305f5ae1e8b3cf57e1721
knc <ruben.fer252@gmail.com>
2024-07-31 04:05:07 +0100

    removed comments and minor errors

--- ChoroplethMap/src/App.jsx
+++ ChoroplethMap/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
@@ -6,7 +7,6 @@ const ChoroplethMap = () => {
     const svgRef = useRef();
 
     useEffect(() => {
-        // Fetch data
         const fetchData = async () => {
             const countyDataUrl =
                 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
@@ -19,14 +19,11 @@ const ChoroplethMap = () => {
             drawChoropleth(countyData, educationData);
         };
 
-        // Draw the choropleth map
         const drawChoropleth = (countyData, educationData) => {
-            // Dimensions and margins
             const margin = { top: 50, right: 20, bottom: 50, left: 20 };
             const width = 960 - margin.left - margin.right;
             const height = 800 - margin.top - margin.bottom;
 
-            // Append SVG element
             const svg = d3
                 .select(svgRef.current)
                 .attr('width', width + margin.left + margin.right)
@@ -34,25 +31,20 @@ const ChoroplethMap = () => {
                 .append('g')
                 .attr('transform', `translate(${margin.left},${margin.top})`);
 
-            // Map and projection
             const path = d3.geoPath();
 
-            // Education data map
             const educationMap = new Map(educationData.map((d) => [d.fips, d]));
 
-            // Color scale
             const color = d3
                 .scaleThreshold()
                 .domain(d3.range(2, 70, 10))
                 .range(d3.schemeBlues[9]);
 
-            // Tooltip
             const tooltip = d3
                 .select('#tooltip')
                 .style('opacity', 0)
                 .style('background-color', 'white');
 
-            // Draw counties
             svg.append('g')
                 .selectAll('path')
                 .data(
@@ -87,7 +79,6 @@ const ChoroplethMap = () => {
                     tooltip.style('opacity', 0);
                 });
 
-            // Legend
             const legendWidth = 300;
             const legendHeight = 10;
 
@@ -133,10 +124,8 @@ const ChoroplethMap = () => {
                 .remove();
         };
 
-        // Fetch and draw the map
         fetchData();
 
-        // Cleanup function
         return () => {
             d3.select('#tooltip').remove();
             d3.select(svgRef.current).selectAll('*').remove();
